8ea0a2301afb7b17e0d29ada3f996324541bd9db
brahimi karim <brahimi.karim@hotmail.fr>
2017-01-26 18:55:07 +0100

    Update ubuntu-install.sh

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -20,9 +20,15 @@ ssh-keygen -t rsa
 copier .pub genereee sur serveur
 sshfs#root@91.134.134.153:/                /media/chip/ovh-web          fuse            port=22,user,allow_other,noatime,_netdev     0 0
 
+
+
 bashrc color
 alias cl='setxkbmap us'
 alias del_spaces='for file in * ; do mv -T "$file" ${file// /_};done'
 alias avconv_all='for f in *; do avconv -i $f $f.mp3 ;done'
 
-unity gnome
+gsettings get com.canonical.Unity.Launcher favorites\\
+/usr/share/applications/
+
+echo "unity gnome ajouter apps backup"
+gsettings set com.canonical.Unity.Launcher favorites "['application://gnome-screenshot.desktop', 'application://gimp.desktop', 'application://openshot.desktop', 'application://xchat.desktop', 'application://firefox.desktop', 'application://nautilus.desktop', 'application://gedit.desktop', 'application://terminator.desktop', 'application://vlc.desktop', 'application://ubuntu-software-center.desktop', 'application://transmission-gtk.desktop', 'application://circuit.desktop', 'application://libreoffice-writer.desktop', 'application://script.desktop', 'unity://running-apps', 'unity://expo-icon', 'unity://devices']"\\
