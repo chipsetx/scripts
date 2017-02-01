4f480ee6a6677f4fa0400eced3ab339abc9ecf23
brahimi karim <brahimi.karim@hotmail.fr>
2017-02-01 17:56:21 +0100

    Update runUbuntu.sh

--- AndroidInstall/runUbuntu.sh
+++ AndroidInstall/runUbuntu.sh
@@ -2,4 +2,7 @@
 #upgrade ubuntu dist sources.list
 su
 busybox mount -o loop /storage/extSdCard/Informatique/Os-images/ubuntu-img/ubuntu.img /mnt
+mount proc
+mount sysfs
 chroot /mnt /root/init.sh
+chroot /mnt /bin/bash -c
