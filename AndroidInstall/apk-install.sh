a5e25020537c3ea2689c2e484a2292932d06f54c
brahimi karim <brahimi.karim@hotmail.fr>
2017-02-03 17:39:38 +0100

    Update apk-install.sh

--- AndroidInstall/apk-install.sh
+++ AndroidInstall/apk-install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #http://whiteboard.ping.se/Android/Debian
-
+https://ge0n0sis.github.io/posts/2016/05/how-to-lock-the-samsung-download-mode-using-an-undocumented-feature-of-aboot/
 
 while read line 
 do 
